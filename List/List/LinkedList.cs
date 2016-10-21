394517f80f836fc7efca5a0cdd0174349c33b970
Chris Janes <gingoreilly@gmail.com>
2016-10-21 10:25:49 +0100

    Add files via upload

---
+++ List/List/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class LinkedList
+    {
+        public class Node
+        {
+            public object Content;
+            public Node Next;
+        }
+
+        private Node head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        public void InsertBeginning(object content)
+        {
+            Node newNode = new Node();
+            newNode.Content = content;
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+        }
+    }
+}
---
+++ List/List/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.InsertBeginning(32);
+            list.InsertBeginning(64);
+            list.InsertBeginning(128);
+            list.InsertBeginning(256);
+        }
+    }
+}
